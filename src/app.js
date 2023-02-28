7e4a41ade9e5342672abdc48cfd1a9a2dfd86050
Diego-1984 <diegomenoruceta@gmail.com>
2023-02-28 20:54:10 +0000

    Creado tapete y carta

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ import "bootstrap";
 import "./style.css";
 
 //declarar carta inicial
+const symbol = ["❤️", "♦️", "♠︎", "♣️"];
+const value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "🤴🏼", "👸🏼", "J"];
 const Card = { symbol: null, value: null };
 
-function getInicialCard() {}
+function getCard() {}
 //obtener numero aleatorio entre 1 y 12
 //obtener el palo aleatoriamente
 //cambiar el estado de tarjeta
