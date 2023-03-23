d706e52c0796601732731d1f27a46cf69374cb07
Diego-1984 <diegomenoruceta@gmail.com>
2023-03-23 12:26:53 +0000

    Falta repetir el simbolo y boton

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 import "bootstrap";
 import "./style.css";
 
-const value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "🤴🏼", "👸🏼", "J"];
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "🤴🏼", "👸🏼", "J"];
 const symbols = ["❤️", "♦️", "♠︎", "♣️"];
 
 const generateSymbol = symbols => {
@@ -11,8 +11,18 @@ const generateSymbol = symbols => {
 };
 
 const getRandomSymbol = () => {
-  const excuse = generateSymbol(symbols);
-  document.getElementById("Ramdomsymbol").innerHTML = excuse;
+  const symbol = generateSymbol(symbols);
+  document.getElementById("Ramdomsymbol").innerHTML = symbol;
 };
 
 getRandomSymbol();
+
+const generateValue = values => {
+  const randomIndex = Math.floor(Math.random() * values.length);
+  return values[randomIndex];
+};
+const getRandomValue = () => {
+  const value = generateValue(values);
+  document.getElementById("Ramdomvalue").innerHTML = value;
+};
+getRandomValue();
